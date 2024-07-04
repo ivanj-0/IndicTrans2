830044c4ef16bff385b426cb4856af2c0174fa2d
Ivan J <100060386+ivanj-0@users.noreply.github.com>
2024-07-04 13:05:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,6 +54,8 @@ git clone https://github.com/pytorch/fairseq.git
 cd fairseq
 sed -i 's/"hydra-core>=1.0.7,<1.1",/"hydra-core>1.0.7",/' setup.py
 sed -i 's/"omegaconf<2.1",/"omegaconf>2.0",/' setup.py
+sudo apt-get update
+sudo apt-get install g++
 python3 -m pip install ./
 cd $root_dir
 
