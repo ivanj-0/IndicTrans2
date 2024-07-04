0e3a63a7e9148785c515918c8ce994639664c731
Ivan J <100060386+ivanj-0@users.noreply.github.com>
2024-07-04 14:28:11 +0530

    Create prepare_finetune_new.sh

---
+++ prepare_finetune_new.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Create directory structure for training and devtest
+mkdir -p indic-indic-exp/train/mun_Deva-hin_Deva
+mkdir -p indic-indic-exp/train/hin_Deva-mun_Deva
+
+mkdir -p indic-indic-exp/devtest/mun_Deva-hin_Deva
+mkdir -p indic-indic-exp/devtest/hin_Deva-mun_Deva
+
+# Copy training files to respective directories
+cp train.mun_Deva indic-indic-exp/train/mun_Deva-hin_Deva/
+cp train.hin_Deva indic-indic-exp/train/mun_Deva-hin_Deva/
+
+cp train.mun_Deva indic-indic-exp/train/hin_Deva-mun_Deva/
+cp train.hin_Deva indic-indic-exp/train/hin_Deva-mun_Deva/
+
+# Copy devtest files to respective directories
+cp dev.mun_Deva indic-indic-exp/devtest/mun_Deva-hin_Deva/
+cp dev.hin_Deva indic-indic-exp/devtest/mun_Deva-hin_Deva/
+
+cp dev.mun_Deva indic-indic-exp/devtest/hin_Deva-mun_Deva/
+cp dev.hin_Deva indic-indic-exp/devtest/hin_Deva-mun_Deva/
+
+# Download and unzip vocabulary file
+curl -L https://indictrans2-public.objectstore.e2enetworks.net/indic-indic-spm.zip -o indic-indic-spm.zip
+unzip indic-indic-spm.zip -d indic-indic-exp/vocab
+rm indic-indic-spm.zip  # Remove the downloaded zip file after extraction
+
+# Download and unzip final_bin file
+curl -L https://indictrans2-public.objectstore.e2enetworks.net/indic-indic-fairseq-dict.zip -o indic-indic-fairseq-dict.zip
+unzip indic-indic-fairseq-dict.zip -d indic-indic-exp/final_bin
+rm indic-indic-fairseq-dict.zip  # Remove the downloaded zip file after extraction
+
+# End of script
