d64fa12bceb15eeacc6f47ca2915fc1d59f33268
Ivan J <100060386+ivanj-0@users.noreply.github.com>
2024-07-05 14:01:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,7 +55,7 @@ cd fairseq
 sed -i 's/"hydra-core>=1.0.7,<1.1",/"hydra-core>1.0.7",/' setup.py
 sed -i 's/"omegaconf<2.1",/"omegaconf>2.0",/' setup.py
 sudo apt-get update
-sudo apt-get install g++
+sudo apt-get install g++ -y
 python3 -m pip install ./
 cd $root_dir
 
